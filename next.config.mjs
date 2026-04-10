d81f0bbe8699b8cf8689f0c227a23890c1b138d6
a1ecsss <estebana.delapena@gmail.com>
2026-04-09 21:45:13 -0600

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  trailingSlash: true,
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
